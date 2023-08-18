d15dd7292a8a6d53a3aa6017b75ab41afcc536c1
Debos <debos.das.02@gmail.com>
2023-08-18 10:09:33 +0600

    first commit

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require("express");
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+const jwt = require("jsonwebtoken");
 require("dotenv").config();
 const cors = require("cors");
 const app = express();
@@ -23,21 +24,102 @@ const client = new MongoClient(uri, {
 	},
 });
 
+function verifyJWT(req, res, next) {
+	const authHeader = req.headers.authorization;
+	if (!authHeader) {
+		return res.status(401).send({ message: "unauthorized access" });
+	}
+	const token = authHeader.split(" ")[1];
+	jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, function (err, decoded) {
+		if (err) {
+			res.status(401).send({ message: "Forbidden access" });
+		} else {
+			req.decoded = decoded;
+			next();
+		}
+	});
+}
+
 async function run() {
 	try {
-		// Connect the client to the server	(optional starting in v4.7)
-		await client.connect();
-		// Send a ping to confirm a successful connection
-		await client.db("admin").command({ ping: 1 });
-		console.log(
-			"Pinged your deployment. You successfully connected to MongoDB!"
-		);
+		const serviceCollection = client.db("geniusCar").collection("services");
+
+		const orderCollection = client.db("geniusCar").collection("orders");
+
+		app.post("/jwt", async (req, res) => {
+			const user = req.body;
+			const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
+				expiresIn: "1d",
+			});
+			res.send({ token });
+			console.log(user);
+		});
+
+		app.get("/services", async (req, res) => {
+			const query = {};
+			const cursor = serviceCollection.find(query);
+			const services = await cursor.toArray();
+			res.send(services);
+		});
+		app.get("/services/:id",  async (req, res) => {
+			const id = req.params.id;
+			const query = { _id: new ObjectId(id) };
+			const service = await serviceCollection.findOne(query);
+			res.send(service);
+		});
+
+		// orders api
+
+		app.get("/orders", verifyJWT, async (req, res) => {
+			console.log(req.headers.authorization);
+			const decoded = req.decoded;
+			console.log("Inside Orders api", decoded);
+			
+			if(decoded.email !==req.query.email){
+				res.status(403).send({message:'unauthorized access' })
+			}
+
+
+			let query = {};
+			if (req.query?.email) {
+				query = {
+					email: req.query.email,
+				};
+			}
+			const cursor = orderCollection.find(query);
+			const orders = await cursor.toArray();
+			res.send(orders);
+		});
+
+		app.post("/orders", verifyJWT, async (req, res) => {
+			const order = req.body;
+			const result = await orderCollection.insertOne(order);
+			res.send(result);
+		});
+
+		app.patch("/orders/:id",verifyJWT, async (req, res) => {
+			const id = req.params.id;
+			const status = req.body.status;
+			const query = { _id: new ObjectId(id) };
+			const updatedDoc = {
+				$set: {
+					status: status,
+				},
+			};
+			const result = await orderCollection.updateOne(query, updatedDoc);
+			res.send(result);
+		});
+
+		app.delete("/orders/:id", verifyJWT, async (req, res) => {
+			const id = req.params.id;
+			const query = { _id: new ObjectId(id) };
+			const result = await orderCollection.deleteOne(query);
+			res.send(result);
+		});
 	} finally {
-		// Ensures that the client will close when you finish/error
-		await client.close();
 	}
 }
-run().catch(console.dir);
+run().catch((error) => console.error(error));
 
 app.get("/", (req, res) => {
 	res.send("Genius car server is running");
